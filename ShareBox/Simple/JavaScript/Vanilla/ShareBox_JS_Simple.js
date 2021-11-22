0033e38b8150670adfad82d11e40c5b98e44dd87
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-21 18:41:04 -0800

    ShareBox_JS_Simple.js ( V1)
    Version 1 of the simple Vanilla JavaScript sharebox dialog box..

---
+++ ShareBox/Simple/JavaScript/Vanilla/ShareBox_JS_Simple.js
@@ -0,0 +1,14 @@
+// Start of script
+// This program is not ready yet. Pseudocode is used to fill in some of the gaps.
+class shareBoxJS_Simple() {
+  fetch platforms from file(unlisted);
+  console.log("If the dialog box is not showing up, try clearing your cache, as it is likely disabled");
+  alert("Share this <mediaType>");
+  alert(platforms());
+  alert("<br>");
+}
+/* File info
+* File type: JavaScript source file (*.js)
+* File version: 1 (2021, Sunday, November 21st at 6:41 pm)
+* Line count (including blank lines and compiler line): 15
+*/ // End of script
